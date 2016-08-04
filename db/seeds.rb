86aca370a2d3581cdaf89bd7b780735374ae849f
kit jonathan ho <j.hokit80@gmail.com>
2016-08-04 11:42:20 +0800

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: "Fresh Food")
+Category.create(name: "Sea Food")
+Category.create(name: "Dairy")
+Category.create(name: "Sea Food")
+Category.create(name: "Alcohol")
+Category.create(name: "Household")
